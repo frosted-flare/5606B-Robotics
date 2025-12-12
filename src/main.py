c1ac473d55c37863991a1e21d77508ab593cfb16
Simon Ward <sww.couldbe@gmail.com>
2025-12-13 09:49:28 +1300

    Added pnumatics code

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ left_motor_group = MotorGroup(left_drive_1,left_drive_2,left_drive_3)
 right_drive_1 = Motor(Ports.PORT4,GearSetting.RATIO_6_1,False)
 right_drive_2 = Motor(Ports.PORT5,GearSetting.RATIO_6_1,False)
 right_drive_3 = Motor(Ports.PORT6,GearSetting.RATIO_6_1,True)
-right_motor_group = MotorGroup(right_drive_1,right_drive_2,left_drive_3)
+right_motor_group = MotorGroup(right_drive_1,right_drive_2,right_drive_3)
 
 ## Drivetrain ##
 
@@ -32,6 +32,24 @@ drivetrain = DriveTrain(left_motor_group,right_motor_group)
 
 drive_toggle = "Tank" # This will set the method of driving between tank and arcade
 drive_speed = 1 # This how fast the robot will drive in percentage
+
+## Other Motors #
+belt_1 = Motor(Ports.PORT7,GearSetting.RATIO_18_1,True)
+outake_1 = Motor(Ports.PORT8,GearSetting.RATIO_18_1,True)
+
+belt_1.set_stopping(HOLD)
+outake_1.set_stopping(HOLD)
+
+belt_1_state = 0
+outake_1_state = 0
+
+loader_pneumatic = Pneumatics(brain.three_wire_port.a)
+goal_pneumatic_state = Pneumatics(brain.three_wire_port.b)
+parking_pneumatic = Pneumatics(brain.three_wire_port.c)
+
+loader_pneumatic_state = False
+goal_pneumatic_state = False
+parking_pneumatic_state = False
 
 ## Timer ##
 timer = Timer()
@@ -43,6 +61,33 @@ current_status_screen = 0
 status_screen_debounce = 400
 
 last_status_update = timer.time()
+
+def toggle_loader_pnumatic():
+    if loader_pneumatic_state == True:
+        loader_pneumatic_state = False
+        loader_pneumatic.close()
+
+    else:
+        loader_pneumatic_state = True
+        loader_pneumatic.open()
+
+def toggle_goal_pnumatic():
+    if goal_pneumatic_state == True:
+        goal_pneumatic_state = False
+        loader_pneumatic.close()
+
+    else:
+        goal_pneumatic_state = True
+        loader_pneumatic.open()
+
+def toggle_parking_pnumatic():
+    if parking_pneumatic_state == True:
+        parking_pneumatic_state = False
+        loader_pneumatic.close()
+
+    else:
+        parking_pneumatic_state = True
+        loader_pneumatic.open()
 
 def toggle_mode():
 
@@ -143,9 +188,33 @@ def update_screen():
         if brain.sdcard.is_inserted() == True:
             brain.screen.draw_image_from_file('logo_small_size.png',380,0)
 
+def move_belt(value):
+    global belt_1_state
+    global belt_1
 
+    if (belt_1_state < 1 and value == 1) or (belt_1_state > -1 and value == -1): 
+        belt_1_state += value
+    belt_1.set_velocity(200)
+    if belt_1_state == 1:
+        belt_1.spin(FORWARD)
+    elif belt_1_state == -1:
+        belt_1.spin(REVERSE)
+    else:
+        belt_1.stop()
 
+def move_outake(value):
+    global outake_1_state
+    global outake_1
 
+    if (outake_1_state < 1 and value == 1) or (outake_1_state > -1 and value == -1): 
+        outake_1_state += value
+    outake_1.set_velocity(100)
+    if outake_1_state == 1:
+        outake_1.spin(FORWARD)
+    elif outake_1_state == -1:
+        outake_1.spin(REVERSE)
+    else:
+        outake_1.stop()
 
 def autonomous():
 
@@ -155,14 +224,25 @@ def autonomous():
 
 def user_control():
     global last_status_update
-    
-    update_screen()
 
-    
+    global belt_1_state
+    global outake_1_state
+
+    update_screen()
 
     brain.screen.pressed(change_status_screen)
 
     controller.buttonX.pressed(toggle_mode)
+
+    controller.buttonL1.pressed(move_belt,(1,))
+    controller.buttonL2.pressed(move_belt,(-1,))
+
+    controller.buttonR1.pressed(move_outake,(1,))
+    controller.buttonR2.pressed(move_outake,(-1,))
+
+    controller.buttonLeft.pressed(toggle_loader_pnumatic)
+    controller.buttonRight.pressed(toggle_goal_pnumatic)
+    controller.buttonUp.pressed(toggle_parking_pnumatic)
 
     # place driver control in this while loop
     while True:
